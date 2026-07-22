a3e988eb6f69714cfeb0ce2fedb3c582dae88c36
hiatus69 <narathip.fluk@gmail.com>
2026-07-22 11:28:45 +0700

    lecture3.1

--- Lacture04/ex-4.py
+++ Lacture04/ex-4.py
@@ -5,10 +5,24 @@
 #     if num % column == 0:
 #         print()
 
-row = int(input('แถวที่อยากได้'))
-row = 100//row
+# row = int(input('แถวที่อยากได้'))
+# row = 100//row
 
-for num in range(2,101,1):
-    print(num ,end=" ")
-    if num % row == 0:
-        print()
+
+# for num in range(1,101,1):
+#     print(num ,end=" ")
+#     if num % row == 0:
+#         print()
+
+#ไม่ผ่าน
+row = int(input('แถวที่อยากได้'))
+count = 1
+for i in range(1,20,1):
+    for num in range(row):
+        if count > 100 :
+            break
+        print(count ,end=" ")
+        count += 1
+    if count > 100:
+        break
+    print()
